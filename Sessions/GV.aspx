000138cf87989d3fd507a5fa0d916ed51db13612
ashishkhadye33 <ashishkhadye33@gmail.com>
2024-07-03 18:46:06 +0530

    session 7

---
+++ Sessions/GV.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/User.Master" AutoEventWireup="true" CodeBehind="GV.aspx.cs" Inherits="Sessions.GV" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="container" style="margin-top:50px">
+        <h2 style="text-align:center">Gridview Example - insert,update,delete</h2>
+
+<asp:GridView ID="gvdata" runat="server" CssClass="table table-striped" AutoGenerateColumns="false" DataKeyNames="Srno"
+        OnRowEditing="gvdata_RowEditing" OnRowCancelingEdit="gvdata_RowCancelingEdit"
+    OnRowUpdating="gvdata_RowUpdating" OnRowDeleting="gvdata_RowDeleting" EmptyDataText="No data found">
+    <Columns>
+        <asp:TemplateField HeaderText="Student Name" ItemStyle-Width="150">
+            <ItemTemplate>
+                <asp:Label ID="lblName" runat="server" Text='<%# Eval("Name") %>'></asp:Label>
+            </ItemTemplate>
+            <EditItemTemplate>
+                <asp:TextBox ID="txteName" runat="server" Text='<%# Eval("Name") %>'></asp:TextBox>
+            </EditItemTemplate>
+        </asp:TemplateField>
+        <asp:TemplateField HeaderText="City" ItemStyle-Width="150">
+            <ItemTemplate>
+                <asp:Label ID="lblCountry" runat="server" Text='<%# Eval("City") %>'></asp:Label>
+            </ItemTemplate>
+            <EditItemTemplate>
+                <asp:TextBox ID="txteCountry" runat="server" Text='<%# Eval("City") %>'></asp:TextBox>
+            </EditItemTemplate>
+        </asp:TemplateField>
+        <asp:CommandField ButtonType="Link" ShowEditButton="true" ShowDeleteButton="true"
+            ItemStyle-Width="150" />
+    </Columns>
+</asp:GridView>
+<table border="1" cellpadding="0" cellspacing="0" style="border-collapse: collapse">
+    <tr>
+        <td style="width: 150px">
+            Name:<br />
+            <asp:TextBox ID="txtname" runat="server" Width="140" />
+        </td>
+        <td style="width: 150px">
+            City:<br />
+            <asp:TextBox ID="txtcity" runat="server" Width="140" />
+        </td>
+        <td style="width: 100px">
+            <asp:Button ID="btnAdd" runat="server" Text="Add" OnClick="btnAdd_Click" />
+        </td>
+    </tr>
+</table>
+    </div>
+</asp:Content>
---
+++ Sessions/GV.aspx.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Sessions
+{
+    public partial class GV : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["connstr"].ConnectionString);
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                gvdata.DataSource=getlist();
+                gvdata.DataBind();
+            }
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            con.Close();
+            SqlCommand cmd = new SqlCommand("insert into GVDemo values ('"+txtname.Text+"','"+txtcity.Text+"')",con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+
+            txtcity.Text = "";
+            txtname.Text = "";
+            gvdata.DataSource = getlist();
+            gvdata.DataBind();
+        }
+
+        protected DataSet getlist()
+        {
+            con.Close();
+            SqlCommand cmd = new SqlCommand("select * from GVDemo",con);
+            con.Open();
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            sda.Fill(ds);
+            return ds;
+        }
+
+        protected void gvdata_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            gvdata.EditIndex = e.NewEditIndex;
+            gvdata.DataSource = getlist();
+            gvdata.DataBind();
+        }
+
+        protected void gvdata_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            gvdata.EditIndex = -1;
+            gvdata.DataSource = getlist();
+            gvdata.DataBind();
+        }
+
+        protected void gvdata_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            con.Close();
+            GridViewRow row = gvdata.Rows[e.RowIndex];
+            string name = (row.FindControl("txteName") as TextBox).Text;
+            string city = (row.FindControl("txteCountry") as TextBox).Text;
+            SqlCommand cmd = new SqlCommand("Update GVDemo set Name='" + name + "',City='" + city + "' where Srno='" + gvdata.DataKeys[e.RowIndex].Value + "'", con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            gvdata.EditIndex = -1;
+            gvdata.DataSource = getlist();
+            gvdata.DataBind();
+
+        }
+
+        protected void gvdata_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            con.Close();
+            SqlCommand cmd = new SqlCommand("Delete from GVDemo where Srno='" + gvdata.DataKeys[e.RowIndex].Value + "'", con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            gvdata.DataSource = getlist();
+            gvdata.DataBind();
+        }
+    }
+}
