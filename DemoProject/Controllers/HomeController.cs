3e1ae2eb249edb9ac4daf2a2ac029ff6726d5bb1
ashishkhadye33 <ashishkhadye33@gmail.com>
2024-07-30 12:00:59 +0530

    task and mvc demo

---
+++ DemoProject/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+using DemoProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DemoProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            int a = 70;
+            return View(a);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ RegisterLogin/Register.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RegisterLogin
+{
+    public partial class Register : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["connstr"].ConnectionString);
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Steps to store form data in database
+            //1.Create UI Form
+            //2.Apply validation to form
+            //3.Create database table with respective columns
+            //4.Setup connection string in web.config file
+            //5.Create connection class in code behind file
+            //6.Write ado.net code to store data in database
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            con.Close();
+            SqlCommand cmd = new SqlCommand("insert into EmpRegister values (@name,@email,@contact,@dept,@pwd)", con);
+            cmd.Parameters.AddWithValue("@name",txtName.Text);
+            cmd.Parameters.AddWithValue("@email", txtEmail.Text);
+            cmd.Parameters.AddWithValue("@contact", txtContact.Text);
+            cmd.Parameters.AddWithValue("@dept", ddldept.SelectedItem.Text);
+            cmd.Parameters.AddWithValue("@pwd", txtPassword.Text);
+            con.Open();
+            cmd.ExecuteNonQuery();
+
+            txtContact.Text = "";
+            txtEmail.Text = "";
+            txtName.Text = "";
+            ddldept.SelectedIndex= 0;
+
+            this.ClientScript.RegisterStartupScript(this.GetType(), "SweetAlert", "swal('Registered Successfully..!','','success');", true);
+
+        }
+    }
+}
